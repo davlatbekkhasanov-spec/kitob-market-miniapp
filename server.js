d5d415ba17ba32afbda69ad22c96b26dc1d74ef4
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-04-09 12:44:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,51 @@ app.get("/", (req, res) => {
     <!DOCTYPE html>
     <html>
       <head>
-        <title>Kitob Mini App</title>
+        <title>Kitob Market</title>
+        <style>
+          body {
+            font-family: Arial;
+            background: #f5f5f5;
+            margin: 0;
+            padding: 20px;
+          }
+          h1 {
+            text-align: center;
+          }
+          .book {
+            background: white;
+            padding: 15px;
+            margin: 10px 0;
+            border-radius: 10px;
+          }
+          button {
+            background: green;
+            color: white;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+          }
+        </style>
       </head>
       <body>
-        <h1>📚 Kitob Mini App ishlayapti!</h1>
-        <p>Bu sizning birinchi Telegram Mini App'ingiz 🚀</p>
+        <h1>📚 Kitoblar do‘koni</h1>
+
+        <div class="book">
+          <h3>Alifbo kitobi</h3>
+          <p>Narxi: 20 000 so‘m</p>
+          <button>Savatga qo‘shish</button>
+        </div>
+
+        <div class="book">
+          <h3>Matematika 1-sinf</h3>
+          <p>Narxi: 25 000 so‘m</p>
+          <button>Savatga qo‘shish</button>
+        </div>
+
       </body>
     </html>
   `);
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server ishlayapti: " + PORT);
-});
+app.listen(PORT);
