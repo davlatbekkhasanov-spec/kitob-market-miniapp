6feadc297f760050854273ec0fdddd979daf6de2
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-04-23 18:14:24 +0500

    Revert "Revert "Serve static frontend, add mini‑app UI, and refactor admin to lightweight order management""

--- src/server.js
+++ src/server.js
@@ -1,4 +1,11 @@
-const { initAndStart } = require('./app');
+const express = require('express');
+const path = require('path');
+const { app, initAndStart } = require('./app');
+
+app.use(express.static(path.join(__dirname, '..')));
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend.html'));
+});
 
 initAndStart().catch((e) => {
   console.error(e);
